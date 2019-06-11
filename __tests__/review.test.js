e2ec9495fb21d02578c40630b20dcc4feebbdb74
daniel-testaguzza <danieltestaguzza@gmail.com>
2019-06-11 10:55:14 -0400

    all test passing

--- __tests__/review.test.js
+++ __tests__/review.test.js
@@ -1,9 +1,39 @@
 const Review = require("../src/review");
 
 describe("Review", () => {
-    test("Should return ID", () => {
-        const underTest = new Review(123);
-        const actual = underTest.id;
-        expect(actual).ToEqual(123);
-    })
-})
+  test("Should return ID", () => {
+    const underTest = new Review(101);
+    const actual = underTest.id;
+    expect(actual).toEqual(101);
+  });
+
+  test("Should return Title", () => {
+    const underTest = new Review(123, "Damn Daniel");
+    const actual = underTest.title;
+    expect(actual).toBe("Damn Daniel");
+  });
+
+  test("Should return imageUrl", () => {
+    const underTest = new Review(123, "Damn Daniel", "www.damndaniel.com");
+    const actual = underTest.imageUrl;
+    expect(actual).toBe("www.damndaniel.com");
+  });
+
+  test("Should return reviewCategory", () => {
+    const underTest = new Review(123, "daniel", "www.damndaniel.com", "meme");
+    const actual = underTest.reviewCategory;
+    expect(actual).toBe("meme");
+  });
+
+  test("Should return content", () => {
+    const underTest = new Review(
+      123,
+      "Damn Daniel",
+      "www.damndaniel.com",
+      "meme",
+      "Daniel is behind"
+    );
+    const actual = underTest.content;
+    expect(actual).toBe("Daniel is behind");
+  });
+});
--- src/review.js
+++ src/review.js
@@ -25,4 +25,4 @@ class Review {
   }
 }
 
-module.export = Review;
+module.exports = Review;

66d226ce833db09a3fad900061e0f4da22e9a304
daniel-testaguzza <danieltestaguzza@gmail.com>
2019-06-17 09:51:25 -0400

    add css

--- src/Server/models/populator.js
+++ src/Server/models/populator.js
@@ -5,16 +5,40 @@ const repo = new ReviewRepository();
 
 function populateReviews() {
   repo.addReview(
-    new Review(1, "test this", "this test", "really works", "watch")
+    new Review(
+      1,
+      "test this",
+      "https://www.lifewire.com/thmb/miKAPcqjSNyQ960gpsFtoEjr17w=/768x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/sstMxMh-5ab00c7bfa6bcc003622e4f5.jpg",
+      "really works",
+      "watch"
+    )
   );
   repo.addReview(
-    new Review(2, "Test title", "Test", "test image url", "test category")
+    new Review(
+      2,
+      "Test title",
+      "https://www.lifewire.com/thmb/miKAPcqjSNyQ960gpsFtoEjr17w=/768x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/sstMxMh-5ab00c7bfa6bcc003622e4f5.jpg",
+      "test image url",
+      "test category"
+    )
   );
   repo.addReview(
-    new Review(3, "test title 2", "test2", "test image url2", "test category2")
+    new Review(
+      3,
+      "test title 2",
+      "https://www.lifewire.com/thmb/miKAPcqjSNyQ960gpsFtoEjr17w=/768x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/sstMxMh-5ab00c7bfa6bcc003622e4f5.jpg",
+      "test image url2",
+      "test category2"
+    )
   );
   repo.addReview(
-    new Review(4, "test title3", "test3", "test image url3", "test category3")
+    new Review(
+      4,
+      "test title3",
+      "https://www.lifewire.com/thmb/miKAPcqjSNyQ960gpsFtoEjr17w=/768x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/sstMxMh-5ab00c7bfa6bcc003622e4f5.jpg",
+      "test image url3",
+      "test category3"
+    )
   );
 }
 

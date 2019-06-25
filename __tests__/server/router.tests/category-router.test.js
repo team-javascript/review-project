683d4eb7e50dc40d1ce69b8497629363a72cf7c0
jon-cunicella <joncunicella@gmail.com>
2019-06-25 11:59:54 -0400

    Tests passing for category router:

---
+++ __tests__/server/router.tests/category-router.test.js
@@ -0,0 +1,57 @@
+const CategoryController = require("../../../src/Server/controllers/category-controller");
+jest.mock("../../../src/Server/controllers/category-controller.js");
+
+const request = require("supertest");
+const app = require("../../../app");
+
+describe("Categories router/controller", () => {
+  test("Should successfully connect to '/categories' ", done => {
+    //Arrange
+    request(app)
+      .get("/categories")
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
+});
+  test("Should render a list of categories for each provided review when route is '/categories' ", done => {
+    //Arrange
+    request(app)
+      .get("/categories")
+      .then(response => {
+        // console.log(response)
+        expect(response.text).toBe(
+          '<!DOCTYPE html>\r\n<html lang="en">\r\n\r\n<head>\r\n   <meta charset="UTF-8">\r\n   <meta name="viewport" content="width=device-width, initial-scale=1.0">\r\n   <meta http-equiv="X-UA-Compatible" content="ie=edge">\r\n   <link rel=\'stylesheet\' type="text/css" href="/stylesheets/style.css" media="all" />\r\n   <title>...Welp...</title>\r\n</head>\r\n\r\n<body>\r\n   <div class="layout__wrapper">\r\n      <header class="layout__header">\r\n         <h1 class="grid-center">...Welp...</h1>\r\n         <nav class="grid-right">\r\n            <ul class="nav__menu__list">\r\n               <li class="nav__menu__list__item"><a href="/reviews">Reviews</a></li>\r\n               <li class="nav__menu__list__item"><a href="/categories">Categories</a></li>\r\n               <li class="nav__menu__list__item"><a href="/tags">Tags</a></li>\r\n            </ul>\r\n         </nav>\r\n      </header>\r\n      <main>\r\n    <form class="category-form" action="/categories/add" method="POST">\r\n        <label>Category: <input type="text" name="category"></label>\r\n            <button class="Submit__button">Submit</button>\r\n    </form>\r\n    <ul class="category__list">\r\n        <li>\r\n            <a href="/categories/">Test category</a>\r\n        </li>\r\n    </ul>\r\n</main>\r\n      <a class="home__button" href="/">Back To Home</a>\r\n      <footer class="layout__footer">\r\n         <small class="layout__footer__copy">&copy; Aquisitions Inc, 2019</small>\r\n      </footer>\r\n   </div>\r\n</body>'
+        );
+        done();
+      });
+  });
+
+//   test("should successfully connect to '/reviews/1'", done => {
+//     // Arrange
+//     request(app)
+//       // Act
+//       .get("/reviews/1")
+//       .then(response => {
+//         // Assert
+
+//         expect(response.statusCode).toBe(200);
+//         done();
+//       });
+//   });
+
+//   test("should render a single review", done => {
+//     // Arrange
+//     request(app)
+//       // Act
+//       .get("/reviews/1")
+//       .then(response => {
+//         // Assert
+//         expect(response.text).toBe(
+//           `<!DOCTYPE html>\r\n<html lang="en">\r\n\r\n<head>\r\n   <meta charset="UTF-8">\r\n   <meta name="viewport" content="width=device-width, initial-scale=1.0">\r\n   <meta http-equiv="X-UA-Compatible" content="ie=edge">\r\n   <link rel=\'stylesheet\' type="text/css" href="/stylesheets/style.css" media="all" />\r\n   <title>...Welp...</title>\r\n</head>\r\n\r\n<body>\r\n   <div class="layout__wrapper">\r\n      <header class="layout__header">\r\n         <h1 class="grid-center">...Welp...</h1>\r\n         <nav class="grid-right">\r\n            <ul class="nav__menu__list">\r\n               <li class="nav__menu__list__item"><a href="/reviews">Reviews</a></li>\r\n               <li class="nav__menu__list__item"><a href="/categories">Categories</a></li>\r\n               <li class="nav__menu__list__item"><a href="/tags">Tags</a></li>\r\n            </ul>\r\n         </nav>\r\n      </header>\r\n      <main>\r\n    <section class="review__card__details">\r\n        <h1 class="review__card__title"></h1>\r\n        <figure><img src= alt=""></figure>\r\n    </section>\r\n    <ul class="review__card__list">\r\n        <li class="review__card__content">Content: </li>\r\n    </ul>\r\n</main>\r\n      <a class="home__button" href="/">Back To Home</a>\r\n      <footer class="layout__footer">\r\n         <small class="layout__footer__copy">&copy; Aquisitions Inc, 2019</small>\r\n      </footer>\r\n   </div>\r\n</body>`
+//         );
+//         done();
+//       });
+//   });
+// });

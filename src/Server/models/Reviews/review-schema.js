e1623dccdac31107a785b4d88d28a35586c34797
Chris-Ross <ChrisJRossJr@gmail.com>
2019-06-26 16:42:12 -0400

    possible chnges

--- src/Server/models/Reviews/review-schema.js
+++ src/Server/models/Reviews/review-schema.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
-
 module.exports = new mongoose.Schema({
   title: String,
   content: String,
-  categories: [{type: mongoose.Schema.Types.ObjectId, ref: 'Category'}],
+  categories: [{ type: mongoose.Schema.Types.ObjectId, ref: "Category" }],
   imageUrl: String,
-  tags: [{type: mongoose.Schema.Types.ObjectId, ref: 'Tag'}],
-  comments: [{type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}]
+  tags: [{ type: mongoose.Schema.Types.ObjectId, ref: "Tag" }],
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
+  rating: { type: Number, default: 0 }
 });
 
 //Why is a new Schema being made both here and in the review.js?

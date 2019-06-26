a5ef0583b66db8c2f5c4db171bbf20558582e540
Chris-Ross <ChrisJRossJr@gmail.com>
2019-06-26 14:59:53 -0400

    Comments section working, ready for merge

--- src/Server/controllers/comment-controller.js
+++ src/Server/controllers/comment-controller.js
@@ -6,17 +6,23 @@ const Review = require("../models/Reviews/review");
 class CommentController {
   static addComment(req, res, next) {
     const comment = req.body.comment;
+    const author = req.body.author;
     const reviewId = req.params.id;
 
     Review.findById(reviewId, (err, review) => {
       const commentToAdd = new Comment({
         input: comment,
+        author,
         review
       });
       review.comments.push(commentToAdd);
       // commentToAdd.reviews.push(reviewId);
 
-      commentToAdd.save((err, comment) => {
+      review.save((err, review) => {
+        if (err) return console.error(err);
+      });
+
+      commentToAdd.save((err, commentToAdd) => {
         if (err) return console.error(err);
         res.redirect(`/reviews/${reviewId}`);
       });
--- src/Server/models/comments/comment-schema.js
+++ src/Server/models/comments/comment-schema.js
@@ -2,5 +2,6 @@ const mongoose = require("mongoose");
 
 module.exports = new mongoose.Schema({
   input: { type: String },
+  author: { type: String },
   review: { type: mongoose.Schema.Types.ObjectId, ref: "Review" }
 });
